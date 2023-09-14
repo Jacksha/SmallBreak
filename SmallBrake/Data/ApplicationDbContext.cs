656e7bb213699a83ed084288a083105547542c1c
Dario Jakovljević <dajakovl@vsite.hr>
2023-09-14 18:18:39 +0200

    Add project files.

---
+++ SmallBrake/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SmallBrake.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
